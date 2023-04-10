254c8b42bfcb7ab3f1d09f58f429021e2a1af186
olga <9672503863olya@mail.ru>
2023-04-10 16:23:14 +0300

    исправлены ошибки

--- Program.cs
+++ Program.cs
@@ -7,3 +7,21 @@
         Console.Write($"m[{i}] = ");
         m[i] = Console.ReadLine();
     }
+
+    string[] m_res = new string[] {};
+
+    int len = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if (m[i].Length <=3)
+        {
+            Array.Resize(ref m_res, len+1);
+            m_res[len] = m[i];
+            len += 1;
+        }
+    }
+    
+    for(int i = 0; i < m_res.Length; i++)
+    {
+        Console.Write($"m_res[{i}] = {m_res[i]} ");
+    }
